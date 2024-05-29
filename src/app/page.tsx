82bc0d43f3a3fda656bbe6b3a8f1406c1bfb55a2
didoslavov <didkoslawow@gmail.com>
2024-05-29 17:43:48 +0300

    add main logic and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,95 @@
-import Image from "next/image";
+'use client';
+
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Progress } from '@/components/ui/progress';
+import { BiCheckDouble } from 'react-icons/bi';
+import Dropzone from '@/components/layout/dropzone';
+
+export type TransformerData = {
+    score: number;
+    label: string;
+    box: {
+        xmin: number;
+        ymin: number;
+        xmax: number;
+        ymax: number;
+    };
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [result, setResult] = useState<TransformerData[] | null>(null);
+    const [progress, setProgress] = useState(0);
+    const [ready, setReady] = useState<boolean | null>(null);
+    const [status, setStatus] = useState('loading');
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const worker = useRef<Worker | null>(null);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    useEffect(() => {
+        if (!worker.current) {
+            worker.current = new Worker(new URL('../lib/worker.ts', import.meta.url), { type: 'module' });
+        }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        const onMessageReceived = (e: { data: { status: string; progress: number; result: TransformerData[] } }) => {
+            switch (e.data.status) {
+                case 'initiate':
+                    setStatus('initiate');
+                    setReady(false);
+                    break;
+                case 'progress':
+                    setStatus('progress');
+                    setProgress(Math.ceil(e.data.progress));
+                    break;
+                case 'ready':
+                    setStatus('ready');
+                    setReady(true);
+                    break;
+                case 'complete':
+                    setStatus('complete');
+                    setResult(e.data.result);
+                    break;
+            }
+        };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        worker.current.addEventListener('message', onMessageReceived);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+        return () => worker.current?.removeEventListener('message', onMessageReceived);
+    });
+
+    const detector = useCallback((image: File) => {
+        if (worker.current) {
+            worker.current.postMessage({ image });
+        }
+    }, []);
+
+    return (
+        <section className="py-12">
+            <div className="container max-w-4xl">
+                <div className="flex justify-between items-end">
+                    <div>
+                        <h1 className="text-4xl font-bold text-zinc-800">Detect objects</h1>
+                        <h3 className="text-lg text-zinc-600">in images using HuggingFace and Transformers.js</h3>
+                    </div>
+                    {ready ? (
+                        <div className="flex font-bold items-center justify-end gap-2 text-emerald-600">
+                            <p>Transformer Ready</p>
+                            <BiCheckDouble className="text-3xl" />
+                        </div>
+                    ) : (
+                        <div className="text-end">
+                            <p>Transformer status</p>
+                            {progress > 0 && !ready && <Progress value={progress} />}
+                        </div>
+                    )}
+                </div>
+                <Dropzone
+                    status={status}
+                    setStatus={setStatus}
+                    detector={detector}
+                    result={result}
+                    setResult={setResult}
+                    className="mt-10 rounded-xl border-teal-950 border-2 border-dashed p-10"
+                />
+            </div>
+        </section>
+    );
 }
