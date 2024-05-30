5e8d12d29a5d3909b50bae9f443778d260accc72
didoslavov <didkoslawow@gmail.com>
2024-05-30 15:46:49 +0300

    abstract the web worker to handle multiple instances for different ai models

--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -5,30 +5,32 @@ env.allowLocalModels = false;
 type ResolvedPipelineInstance = Awaited<ReturnType<typeof pipeline>>;
 
 interface PipelineOptions {
-    progress?: boolean;
+    progress: boolean;
 }
 
-class Pipeline {
-    static task: PipelineType = 'object-detection';
-    static model = 'Xenova/detr-resnet-50';
-    static instance: ResolvedPipelineInstance | null = null;
+class PipelineSingleton {
+    static instance: Map<string, ResolvedPipelineInstance> = new Map();
 
-    static async getInstance(progress_callback: Function | undefined): Promise<ResolvedPipelineInstance> {
-        if (!this.instance) {
-            this.instance = await pipeline(this.task, this.model, { progress_callback });
+    static async getInstance(task: PipelineType, model: string): Promise<ResolvedPipelineInstance> {
+        const key = `${task}-${model}`;
+        if (!this.instance.has(key)) {
+            const instance = await pipeline(task, model, {
+                progress_callback: (progress: number) => {
+                    self.postMessage(progress);
+                },
+            });
+            this.instance.set(key, instance);
         }
-        return this.instance;
+        return this.instance.get(key)!;
     }
 }
 
-type DetectorFunction = (input: string, options: PipelineOptions) => Promise<any>;
+type DetectorFunction = (input: string, options?: PipelineOptions) => Promise<ResolvedPipelineInstance>;
 
-self.addEventListener('message', async (event: MessageEvent<{ image: string }>) => {
-    const detector = await Pipeline.getInstance((progress: string) => {
-        self.postMessage(progress);
-    });
-    const detectorFunc: DetectorFunction = detector as DetectorFunction;
-    const result = await detectorFunc(event.data.image, { progress: true });
+self.addEventListener('message', async (event: MessageEvent<{ input: string; task: PipelineType; model: string }>) => {
+    const detector = await PipelineSingleton.getInstance(event.data.task, event.data.model);
+    const detectorFunc: DetectorFunction = detector as unknown as DetectorFunction;
+    const result = await detectorFunc(event.data.input);
 
     self.postMessage({ status: 'complete', result });
 });
