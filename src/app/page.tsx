3c467c0a399f7d322e7aeb57ee60e61b93339ab3
didoslavov <didkoslawow@gmail.com>
2024-05-30 16:26:03 +0300

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
 import React from 'react';
+import huggingface from '/public/huggingface.jpg';
+import Link from 'next/link';
 
 const page = () => {
-    return <h1>Home</h1>;
+    return (
+        <section className="text-teal-950 text-center mt-20 max-w-3xl mx-auto">
+            <h1 className="text-3xl font-bold mb-8">Welcome to Visionarai</h1>
+            <p className="text-lg text-teal-800">This is a playground for experimenting with HuggingFace and Transformers.js.</p>
+            <Link href="https://huggingface.co" target="_blank">
+                <Image src={huggingface} alt="hero" width={800} height={500} className="rounded-xl mt-20" />
+            </Link>
+        </section>
+    );
 };
 
 export default page;
--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -3,9 +3,16 @@ import Link from 'next/link';
 const SideNav = () => {
     return (
         <div className="h-screen bg-teal-950 shadow-lg text-lime-50 w-64 space-y-6 py-7 px-2 inset-y-0 transform transition-transform duration-200 ease-in-out">
-            <h2 className="text-2xl font-extrabold tracking-tight">Models</h2>
+            <div className="flex w-full justify-between px-2 items-center">
+                <h2 className="text-2xl font-extrabold tracking-tight">Models</h2>
+                <Link
+                    href="/"
+                    className="text-lime-50 font-bold hover:text-lime-200 transition-colors duration-300 px-3 py-2 rounded-md">
+                    Home
+                </Link>
+            </div>
             <nav>
-                <ul className="space-y-2">
+                <ul className="space-y-4">
                     <li>
                         <Link
                             href="/object-detection"
