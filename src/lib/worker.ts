e215e0b432f403e3f6a49382a18894d8f4edcc29
didoslavov <didkoslawow@gmail.com>
2024-05-31 14:03:36 +0300

    add vector search ai model in web worker

--- src/lib/worker.ts
+++ src/lib/worker.ts
@@ -1,23 +1,30 @@
-import { pipeline, env, type PipelineType } from '@xenova/transformers';
+import { pipeline, env, type PipelineType, type Tensor } from '@xenova/transformers';
 
 env.allowLocalModels = false;
 
 type ResolvedPipelineInstance = Awaited<ReturnType<typeof pipeline>>;
 
 interface PipelineOptions {
-    progress: boolean;
+    progress?: boolean;
+    pooling?: 'mean' | 'max' | 'cls';
+    normalize?: boolean;
 }
 
 class PipelineSingleton {
     static instance: Map<string, ResolvedPipelineInstance> = new Map();
 
-    static async getInstance(task: PipelineType, model: string): Promise<ResolvedPipelineInstance> {
+    static async getInstance(
+        task: PipelineType,
+        model: string,
+        { quantized }: { quantized?: boolean }
+    ): Promise<ResolvedPipelineInstance> {
         const key = `${task}-${model}`;
         if (!this.instance.has(key)) {
             const instance = await pipeline(task, model, {
                 progress_callback: (progress: number) => {
                     self.postMessage(progress);
                 },
+                quantized,
             });
             this.instance.set(key, instance);
         }
@@ -27,10 +34,32 @@ class PipelineSingleton {
 
 type DetectorFunction = (input: string, options?: PipelineOptions) => Promise<ResolvedPipelineInstance>;
 
-self.addEventListener('message', async (event: MessageEvent<{ input: string; task: PipelineType; model: string }>) => {
-    const detector = await PipelineSingleton.getInstance(event.data.task, event.data.model);
-    const detectorFunc: DetectorFunction = detector as unknown as DetectorFunction;
-    const result = await detectorFunc(event.data.input);
+self.addEventListener('message', async (event) => {
+    const { input, task, model } = event.data;
 
-    self.postMessage({ status: 'complete', result });
+    switch (task) {
+        case 'object-detection':
+            const detector = await PipelineSingleton.getInstance(task, model, {});
+            const detectorFunc: DetectorFunction = detector as unknown as DetectorFunction;
+            const result = await detectorFunc(input);
+
+            self.postMessage({ status: 'complete', result });
+            break;
+        case 'feature-extraction':
+            const extractor = await PipelineSingleton.getInstance(task, model, { quantized: false });
+            const extractorFunc: DetectorFunction = extractor as unknown as DetectorFunction;
+            const embeddings = await Promise.all(
+                input.map(async (text: string) => {
+                    const output = await extractorFunc(text, { pooling: 'mean', normalize: true });
+
+                    if ('data' in output) {
+                        return Array.from(output.data as ArrayLike<unknown>) as unknown as number[];
+                    }
+                })
+            );
+            self.postMessage({ status: 'complete', result: embeddings });
+            break;
+        default:
+            console.error(`Unknown task: ${task}`);
+    }
 });
