131581e5e2d866d08ba6b95133344503dcf8b8d6
didoslavov <didkoslawow@gmail.com>
2024-05-31 20:25:36 +0300

    add button for loading sample data

--- src/components/layout/embedded-search.tsx
+++ src/components/layout/embedded-search.tsx
@@ -1,14 +1,17 @@
 'use client';
+
+import { EmbeddingResult } from '@/app/semantic-search/page';
+import { sampleData } from '@/constants/sample-data';
 import { useState } from 'react';
 
-type TextAreaProps = {
+type EmbeddedSearchProps = {
     status: string;
     setStatus: (status: string) => void;
     onExtractFeatures: (texts: string[], query: string) => void;
-    result: string[] | null;
+    result: EmbeddingResult | null;
 };
 
-const TextArea = ({ status, onExtractFeatures, result, setStatus }: TextAreaProps) => {
+const EmbeddedSearch = ({ status, onExtractFeatures, result, setStatus }: EmbeddedSearchProps) => {
     const [text, setText] = useState('');
     const [query, setQuery] = useState('');
 
@@ -18,6 +21,15 @@ const TextArea = ({ status, onExtractFeatures, result, setStatus }: TextAreaProp
         onExtractFeatures(texts, query);
     };
 
+    const handleLoadSampleData = () => {
+        setText(
+            sampleData
+                .map((line) => line.trim())
+                .filter((line) => line.trim() !== '')
+                .join('\n')
+        );
+    };
+
     return (
         <>
             <div className="mt-10">
@@ -25,27 +37,34 @@ const TextArea = ({ status, onExtractFeatures, result, setStatus }: TextAreaProp
                     value={text}
                     onChange={(e) => setText(e.target.value)}
                     placeholder="Enter your texts here, one per line"
-                    className="w-full h-64 p-4 resize-none rounded-xl border border-teal-950 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-transparent transition-colors duration-300"
+                    className="w-full h-64 p-4 resize-none rounded-xl border border-teal-950 focus:outline-none focus:ring-2 bg-lime-50 focus:ring-teal-500 focus:border-transparent transition-colors duration-300"
                 />
                 <input
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
                     placeholder="Enter your search query here"
-                    className="w-full p-4 mt-4 rounded-xl border border-teal-950 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-transparent transition-colors duration-300"
+                    className="w-full p-4 mt-4 rounded-xl border border-teal-950 focus:outline-none focus:ring-2 bg-lime-50 focus:ring-teal-500 focus:border-transparent transition-colors duration-300"
                 />
-                <button
-                    onClick={handleExtractFeatures}
-                    className="mt-4 px-6 py-2 bg-teal-700 hover:bg-teal-800 text-lime-50 rounded-xl">
-                    Search
-                </button>
+                <div className="flex w-full justify-between">
+                    <button
+                        onClick={handleExtractFeatures}
+                        className="mt-4 px-6 py-2 bg-teal-700 hover:bg-teal-800 font-semibold focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-transparent transition-colors duration-300 text-lime-50 rounded-xl">
+                        Search
+                    </button>
+                    <button
+                        onClick={handleLoadSampleData}
+                        className="mt-4 px-6 py-2 bg-teal-400 hover:bg-teal-500 font-semibold focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-transparent transition-colors duration-300 text-teal-950 rounded-xl">
+                        Load Sample
+                    </button>
+                </div>
             </div>
             {status === 'processing' && <div className="mt-4 text-2xl text-teal-950 font-bold">Processing...</div>}
             {status === 'complete' && result && (
                 <div className="mt-4">
                     <h2 className="text-xl font-bold">Results:</h2>
-                    {result.map((text, i) => (
+                    {result.map(({ text, similarity }, i) => (
                         <div key={i}>
-                            <h3 className="text-lg font-semibold">Text {i + 1}:</h3>
+                            <h3 className="text-lg font-semibold">Similarity of {similarity}%</h3>
                             <p className="bg-gray-200 p-2 rounded">{text}</p>
                         </div>
                     ))}
@@ -55,4 +74,4 @@ const TextArea = ({ status, onExtractFeatures, result, setStatus }: TextAreaProp
     );
 };
 
-export default TextArea;
+export default EmbeddedSearch;
