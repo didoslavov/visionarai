7f110c2247d73afce88ed80538410ee24b29c08b
didoslavov <didkoslawow@gmail.com>
2024-05-31 14:02:53 +0300

    add utils to calculate the distance between vectors

---
+++ src/utils/cosine-similarity.ts
@@ -0,0 +1,7 @@
+export function cosineSimilarity(a: number[] | number, b: number[] | number): number {
+    const dotProduct = a.reduce((prev, current, i) => prev + current * (b[i] || 0), 0);
+    const magnitudeA = Math.sqrt(a.reduce((prev, current) => prev + Math.pow(current, 2), 0));
+    const magnitudeB = Math.sqrt(b.reduce((prev, current) => prev + Math.pow(current, 2), 0));
+
+    return dotProduct / (magnitudeA * magnitudeB);
+}
